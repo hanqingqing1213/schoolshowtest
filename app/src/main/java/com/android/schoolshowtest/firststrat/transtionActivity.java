9f0aec68f8af48c62be0669993f07be4451ccb20
Your Name <1163709280@qq.com>
2017-02-22 15:04:12 +0800

    动画完成

--- app/src/main/java/com/android/schoolshowtest/firststrat/transtionActivity.java
+++ app/src/main/java/com/android/schoolshowtest/firststrat/transtionActivity.java
@@ -1,10 +1,51 @@
 package com.android.schoolshowtest.firststrat;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import com.android.schoolshowtest.MainActivity;
+import com.android.schoolshowtest.R;
 
 /**
  * Created by Administrator on 2017/2/22.
  */
 
+
+//判断程序是否第一次运行，如果是则跳转到Viewactivity，不是则跳转到mainactivity
+
 public class transtionActivity extends Activity {
+    boolean isFirstIn=false;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.start);
+
+        final SharedPreferences sharedPreferences=getSharedPreferences("is_first_in_data",MODE_PRIVATE);
+        isFirstIn=sharedPreferences.getBoolean("isFirstIn",true);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isFirstIn){
+                    intent =new Intent(transtionActivity.this, ViewActivity.class);
+                    transtionActivity.this.startActivity(intent);
+                    transtionActivity.this.finish();
+
+
+                } else {
+                    intent =new Intent(transtionActivity.this,MainActivity.class);
+                    transtionActivity.this.startActivity(intent);
+                    transtionActivity.this.finish();
+
+                }
+            }
+        },2000);
+    }
 }
